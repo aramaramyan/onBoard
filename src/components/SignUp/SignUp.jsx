08b0ac204cdbf71e2354cc1aa2f9ad65d2cf7cd7
aramaramyan <aramaramyan1997@mail.ru>
2022-03-28 12:43:38 +0400

    feat: creare SignIn component

---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,15 @@
+import "./SignUp.css";
+
+export default function SignUp() {
+  return (
+    <div className="container_form container_signup">
+      <form action="#" className="form" id="form1">
+        <h2 className="form_title">Sign Up</h2>
+        <input type="text" placeholder="User" className="input"/>
+        <input type="email" placeholder="Email" className="input"/>
+        <input type="password" placeholder="Password" className="input"/>
+        <button className="btn">Sign Up</button>
+      </form>
+    </div>
+  );
+}

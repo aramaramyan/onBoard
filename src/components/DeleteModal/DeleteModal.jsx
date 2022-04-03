75d2b45c027191c2ba30c72065d33e6305e29db2
aramaramyan <aramaramyan1997@mail.ru>
2022-04-03 10:08:02 +0400

    feat: add DeleteModal component

---
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -0,0 +1,38 @@
+import {createPortal} from "react-dom";
+import {deleteBoard} from "../../features/userSlice";
+import {useNavigate} from "react-router";
+import {useDispatch} from "react-redux";
+import dangerIcon from "./../../icons/danger.svg";
+import "./DeleteModal.css";
+
+
+
+export default function DeleteModal({ id, title,toggleModal }) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  function delBoard(id) {
+    dispatch(deleteBoard(id));
+    navigate("/boards");
+  }
+
+  return createPortal(
+    <div className="modal">
+      <div className="modal_main_wrapper" onClick={toggleModal}>
+        <div className="danger_modal_wrapper">
+          <div className="danger_modal_title">
+            <img src={dangerIcon} alt="Danger Icon"/>
+            <div className="danger_modal_text">
+              <p className="danger_modal_firstText">Are you sure you want to delete "{title}" board?</p>
+              <p className="danger_modal_secondText">This board will be deleted immediately.<br/>You can't undo this action</p>
+            </div>
+          </div>
+          <div className="danger_modal_actions">
+            <button onClick={toggleModal}>CANCEL</button>
+            <button onClick={() => delBoard(id)}>DELETE</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  , document.body);
+}
--- src/pages/Board/Board.jsx
+++ src/pages/Board/Board.jsx
@@ -11,77 +11,84 @@ import Navbar from "../../components/Navbar/Navbar";
 import "./Board.css";
 import AsideBoard from "../../components/AsideBoard/AsideBoard";
 import AddList from "../../components/AddList/AddList";
+import DeleteModal from "../../components/DeleteModal/DeleteModal";
 
 export default function Board() {
   const {fullName, boards} = useSelector(state => state.user);
   const [avatarBG, setAvatarBG] = useState("");
-  const { boardID } = useParams();
-  const [board] =boards.filter(board => board.id === boardID);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const {boardID} = useParams();
+  const [board] = boards.filter(board => board.id === boardID);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   useEffect(() => {
     setAvatarBG(randomColor());
   }, []);
 
-  function delBoard(id) {
-    dispatch(deleteBoard(id));
-    navigate("/boards");
+  function toggleModal() {
+    setIsModalOpen(!isModalOpen);
   }
 
   return (
-    <div className="board_page_wrapper" style={{background: `url(${board.bg.main}) center center/cover no-repeat fixed`}}>
-      <Navbar />
-      <aside>
-        <div className="user_info">
-          <div className="first_letter" style={{background: avatarBG, color: "#ffffff"}}>
-            {fullName.slice(0, 1).toUpperCase()}
+    <>
+      <div className="board_page_wrapper"
+           style={{background: `url(${board.bg.main}) center center/cover no-repeat fixed`}}>
+        <Navbar/>
+        <aside>
+          <div className="user_info">
+            <div className="first_letter" style={{background: avatarBG, color: "#ffffff"}}>
+              {fullName.slice(0, 1).toUpperCase()}
+            </div>
+            <div className="user_name">
+              <p>{fullName}'s </p>
+              <p>Workspace</p>
+            </div>
           </div>
-          <div className="user_name">
-            <p>{fullName}'s </p>
-            <p>Workspace</p>
+          <div>
+            <div className="user_boards_title">
+              <img src={boardIcon} alt="Boards Icon" className="user_boards_title_icon"/>
+              <h3>Boards:</h3>
+            </div>
+            {boards.map(board => {
+              return <AsideBoard
+                key={board.id}
+                id={board.id}
+                title={board.title}
+                isFavorite={board.isFavorite}
+              />
+            })}
           </div>
-        </div>
-        <div>
-          <div className="user_boards_title">
-            <img src={boardIcon} alt="Boards Icon" className="user_boards_title_icon"/>
-            <h3>Boards:</h3>
+        </aside>
+        <div className="main_content">
+          <div className="main_content_header">
+            <h3>{board.title}</h3>
+            {board.isFavorite ?
+              <img
+                src={starFillIcon}
+                alt="Star Icon"
+                className="main_content_header_starIcon"
+                onClick={() => dispatch(handleFavorite(board.id))}
+              />
+              : <img
+                src={starIcon}
+                alt="Star Icon"
+                className="main_content_header_starIcon"
+                onClick={() => dispatch(handleFavorite(board.id))}
+              />}
+            <div className="delete_board" onClick={toggleModal}>
+              <h5>DELETE BOARD</h5>
+              <img src={trashIcon} alt="Trash Icon" className="delete_board_icon"/>
+            </div>
           </div>
-          {boards.map(board => {
-            return <AsideBoard
-              key={board.id}
-              id={board.id}
-              title={board.title}
-              isFavorite={board.isFavorite}
-            />
-          })}
-        </div>
-      </aside>
-      <div className="main_content">
-        <div className="main_content_header">
-          <h3>{board.title}</h3>
-          {board.isFavorite?
-            <img
-              src={starFillIcon}
-              alt="Star Icon"
-              className="main_content_header_starIcon"
-              onClick={() => dispatch(handleFavorite(board.id))}
-            />
-            : <img
-              src={starIcon}
-              alt="Star Icon"
-              className="main_content_header_starIcon"
-              onClick={() => dispatch(handleFavorite(board.id))}
-            />}
-          <div className="delete_board" onClick={() => delBoard(board.id)}>
-            <h5>DELETE BOARD</h5>
-            <img src={trashIcon} alt="Trash Icon" className="delete_board_icon"/>
+          <div className="lists">
+            <AddList/>
           </div>
-        </div>
-        <div className="lists">
-          <AddList />
         </div>
       </div>
-    </div>
+      {isModalOpen && <DeleteModal
+        id={board.id}
+        title={board.title}
+        toggleModal={toggleModal}/>}
+    </>
   );
 }

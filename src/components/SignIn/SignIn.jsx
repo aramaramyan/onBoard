f7e3e224ec0a7009ebcef69f7c30bc103594b0d9
aramaramyan <aramaramyan1997@mail.ru>
2022-04-22 12:34:09 +0400

    modify: refactot SignIn state

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,22 +1,28 @@
-import { useSelector, useDispatch } from "react-redux";
-import { setEmail, setPassword, setUserID } from "../../features/signInSlice";
-import signInEmail from "../../services/signInEmail";
-import "./../../pages/Registration/Registration.css";
+import {useState} from "react";
 import {useNavigate} from "react-router";
+import signInEmail from "../../services/signInEmail";
+import setStorage from "../../helpers/setStorage";
+import "./../../pages/Registration/Registration.css";
 
 export default function SignIn() {
-  const email = useSelector(state => state.signIn.email);
-  const password = useSelector(state => state.signIn.password);
-  const userID = useSelector(state => state.signIn.userID);
-  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
+
+  function emailHandler(value) {
+    setEmail(value);
+  }
+
+  function passwordHandler(value) {
+    setPassword(value);
+  }
 
 
   function formSubmit(evt) {
     evt.preventDefault();
 
     signInEmail({ email: email, password: password }).then((res) => {
-      dispatch(setUserID(res.user.uid));
+      setStorage(res.user.uid);
       navigate("/");
     });
   }
@@ -30,14 +36,14 @@ export default function SignIn() {
           type="email"
           placeholder="Email"
           className="input"
-          onChange={evt => dispatch(setEmail(evt.target.value))}
+          onChange={evt => emailHandler(evt.target.value)}
         />
         <input
           value={password}
           type="password"
           placeholder="Password"
           className="input"
-          onChange={evt => dispatch(setPassword(evt.target.value))}
+          onChange={evt => passwordHandler(evt.target.value)}
         />
         <a href="#" className="link">Forgot your password?</a>
         <button className="btn" >Sign In</button>
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import signInReducer from "../features/signInSlice";
 import userReducer from "../features/userSlice";
 
 export const store = configureStore({
   reducer: {
-    signIn: signInReducer,
     user: userReducer
   }
 });
